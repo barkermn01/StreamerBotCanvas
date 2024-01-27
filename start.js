0144259017ad08caf93e80db20abb78de4a85417
Martin Barker <martin-barker@hotmail.co.uk>
2024-01-27 00:22:29 +0000

    inital commit

---
+++ start.js
@@ -0,0 +1,61 @@
+import http from "http";
+import https from 'https'; 
+import fs from 'fs';
+import mime from 'mime';
+import path from 'path';
+import crypto from 'crypto';
+import { fileURLToPath } from 'url';
+
+const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+const leftTrim = (str, chars) => str.replace(new RegExp("^[" + chars + "+]"), "");
+
+const downloadFile = async (url, fileFullPath) =>{
+    console.info('downloading file from url: '+url)
+    return new Promise((resolve, reject) => {
+        https.get(url, (resp) => {
+
+            // chunk received from the server
+            resp.on('data', (chunk) => {
+                fs.appendFileSync(fileFullPath, chunk);
+            });
+
+            // last chunk received, we are done
+            resp.on('end', () => {
+                resolve('File downloaded and stored at: '+fileFullPath);
+            });
+
+        }).on("error", (err) => {
+            reject(new Error(err.message))
+        });
+    })
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const webRoot = ((config.webroot[0] === "." && config.webroot[0] === ".") ? config.webroot.replace("./", __dirname.replaceAll("\\", "/") + "/") : config.webroot) + "/";
+const fileStore = ((config.fileStore[0] === "." && config.fileStore[0] === ".") ? config.fileStore.replace("./", __dirname.replaceAll("\\", "/") + "/") : config.fileStore) + "/";
+
+
+const requestListener = async function (req, res) {
+    let file = leftTrim(req.url, '/');
+    if (file === "") {
+        file = "index.html"
+    }
+    file = webRoot + file;
+
+    try {
+        const fileContent = fs.readFileSync(file);
+        res.writeHead(200, { "Content-Type": mime.getType(file) });
+        res.end(fileContent);
+        return;
+    } catch (e) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 File Not Found");
+        return;
+    }
+};
+
+const server = http.createServer(requestListener);
+server.listen(config.port, config.host, () => {
+    console.log(`Server is running on http://${config.host}:${config.port}`);
+});
---
+++ www/game.js
@@ -0,0 +1,97 @@
+const animationTime = 10;
+
+let lastFrameTime = Date.now();
+let currentFrameTime = Date.now();
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+window.onresize = () => {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+};
+
+
+const rndInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
+class Emote {
+    isFinished = false;
+    isStarted = false;
+
+    top = rndInt(0, windowHeight);
+    left = rndInt(0, windowWidth);
+    width = 128;
+    height = 128;
+
+    #moveVertical = rndInt(50, 500)-windowHeight;
+    #moveHorizontal = rndInt(50, 500)-windowWidth;
+    #image = new Image();
+    #animationTimeLeft = animationTime;
+
+    constructor(path) {
+        this.#image.src = path;
+    }
+
+    update(dt) {
+        if(this.#animationTimeLeft <= 2){
+            if(this.width >= 0 || this.height >= 0){
+                this.width -= 50*dt;
+                this.height -= 50*dt;
+                this.top += 25*dt;
+                this.left += 25*dt;
+            }else{
+                this.isFinished = true;
+            }
+        }else{
+            this.top += this.#moveVertical * dt;
+            this.left += this.#moveHorizontal * dt;
+            if (this.top + this.height >= windowHeight || this.top <= 0) {
+                this.#moveVertical = 0 - this.#moveVertical;
+            }
+            if (this.left + this.width >= windowWidth || this.left <= 0) {
+                this.#moveHorizontal = 0 - this.#moveHorizontal;
+            }
+            this.#animationTimeLeft -= dt;
+        }
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.#image, this.left, this.top, this.width, this.height);
+    }
+}
+
+window.emotes = [new Emote("https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_d872a800ebf6498184d275fd0933cc78/default/dark/3.0")];
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("canvas").width = windowWidth;
+    document.getElementById("canvas").height = windowHeight;
+
+    function primaryLoop() {
+        currentFrameTime = Date.now();
+        const ctx = document.getElementById("canvas").getContext("2d");
+        ctx.clearRect(0, 0, windowWidth, windowHeight);
+
+        deltaTime = (currentFrameTime - lastFrameTime) / 1000;
+        emotes.forEach((emote, idx) => { 
+            emote.update(deltaTime); 
+            if(!emote.isFinished){
+                emote.draw(ctx);  
+            }else{
+                delete emotes[idx];
+            }
+        });
+        emotes = emotes.filter(emote => !emote.isFinished);
+        lastFrameTime = currentFrameTime;
+        window.requestAnimationFrame(primaryLoop);
+    };
+
+    window.requestAnimationFrame(primaryLoop);
+});
+
+const client = new StreamerbotClient({
+    host: '127.0.0.1',
+    port: 9090,
+    endpoint: '/'
+});
+
+client.on('General.Custom', (data) => {
+    console.log('Custom Event:', data);
+});
