7026edfe2882114705e6abe01fdda1dc86c77115
Martin Barker <martin-barker@hotmail.co.uk>
2024-02-22 20:20:25 +0000

    Updated the emote wall to not leave memory locked while unable to load emote, updated engine core to have error system and updated codebase to support libraries

--- www/config.js
+++ www/config.js
@@ -1,13 +1,30 @@
 const Config  = {
     // modules are render in order of top to bottom
     Modules:[
-        "emote"
+        "emote",
+        "chat"
     ],
     emote:{
-        AnimationTime:10,
+        // constant number as a fixed or MinMax Object for random in range
+        AnimationTime:{
+            Min:10,
+            Max:20
+        },
         Speed:{
             Min:100,
-            Max:500
+            Max:300
+        },
+        RandomDirectionsFromStart:true
+    },
+    chat:{
+        MessageArea:{
+            top:0,
+            left:0,
+            right:0,
+            bottom:0,
+            // if 0 will be full width unless fixed information provided to the message area
+            width:0, 
+            height:0
         }
     }
 }
--- www/engine/emote.js
+++ www/engine/emote.js
@@ -1,5 +1,7 @@
 class Emote {
     isFinished = false;
+    isReady = false;
+    Expires = new Date()+30000
 
     width = 96;
     height = 96;
@@ -7,13 +9,43 @@ class Emote {
     left = rndInt(this.height, window.innerWidth)-this.height;
 
 
-    #moveVertical = rndInt(Config.emote.Speed.Min, Config.emote.Speed.Min);
-    #moveHorizontal = rndInt(Config.emote.Speed.Min, Config.emote.Speed.Min);
+    #moveVertical;
+    #moveHorizontal;
     #image = new Image();
-    #animationTimeLeft = Config.emote.AnimationTime;
+    #animationTimeLeft;
 
     constructor(path) {
         this.#image.src = path;
+        this.#image.addEventListener("load", () => { this.isReady = true; })
+
+        // if fixed speed is provided for movement
+        try{
+            const speedType = typeof(Config.emote.Speed);
+            if( speedType == "object" && typeof Config.emote.Speed.Min == 'number' && typeof Config.emote.Speed.Max == 'number'){
+                this.#moveVertical = MinMax.FromObject(Config.emote.Speed).GetValueInRange();
+                this.#moveHorizontal = MinMax.FromObject(Config.emote.Speed).GetValueInRange();
+            }else if(speedType == "number"){
+                this.#moveVertical = MinMax.FromStatic(Config.emote.Speed).GetValueInRange();
+                this.#moveHorizontal = MinMax.FromStatic(Config.emote.Speed).GetValueInRange();
+            }
+
+            const animationTimeType = typeof(Config.emote.AnimationTime);
+            if( animationTimeType == "object" && typeof Config.emote.AnimationTime.Min == 'number' && typeof Config.emote.AnimationTime.Max == 'number'){
+                this.#animationTimeLeft = MinMax.FromObject(Config.emote.AnimationTime).GetValueInRange();
+            }else if(animationTimeType == "number"){
+                this.#animationTimeLeft = MinMax.FromStatic(Config.emote.AnimationTime).GetValueInRange();
+            }
+
+            if(Config.emote.RandomDirectionsFromStart){
+                const flipVert = rndInt(0, 100), flipHori = rndInt(0,100);
+                if(flipVert <= 50){
+                    this.#moveVertical = 0-this.#moveVertical
+                }
+                if(flipHori <= 50){
+                    this.#moveHorizontal = 0-this.#moveHorizontal
+                }
+            }
+        }catch(err){ ShowError(err, true); console.error(err); }
     }
 
     update(dt) {
@@ -39,7 +71,13 @@ class Emote {
     }
 
     draw(ctx) {
-        ctx.drawImage(this.#image, this.left, this.top, this.width, this.height);
+        if(this.isReady){
+            ctx.drawImage(this.#image, this.left, this.top, this.width, this.height);
+        }else{
+            if(new Date() > this.Expires){
+                this.isFinished = true;
+            }
+        }
     }
 }
 
